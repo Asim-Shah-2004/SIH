b6a033668dbb6d776ab2e61169fab413894bacb0
javascript <ntpjc2vinayak@gmail.com>
2024-11-26 19:14:24 +0530

    added requirements.txt via pipreqs .

--- ML/ML/settings.py
+++ ML/ML/settings.py
@@ -124,8 +124,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',   # React development server
     # Add other origins as needed
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
