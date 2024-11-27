70cd588aed5f4cd17c0a968b11a1d8bf027b235e
SoulDev07 <mayankravariya@gmail.com>
2024-11-27 12:47:04 +0530

    added nativewind

---
+++ client/app/_layout.jsx
@@ -0,0 +1,46 @@
+import "../global.css";
+
+import { useEffect } from 'react'
+import { LightTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
+import { Slot, SplashScreen } from 'expo-router'
+
+import HomePage from './index.jsx'
+import { SafeAreaView } from "react-native";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router'
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+}
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+  })
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) {
+    return null
+  }
+
+  return (
+    <ThemeProvider value={LightTheme}>
+      <SafeAreaView className="flex-1">
+        <HomePage />
+      </SafeAreaView>
+    </ThemeProvider>
+  )
+}
