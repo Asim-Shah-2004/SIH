bafe88f8949f805a112eb7fd327cba598037431d
asim <asimshah2111@gmail.com>
2024-12-09 20:43:51 +0530

    index.js fix

--- server/Models/index.js
+++ server/Models/index.js
@@ -1,15 +1,10 @@
-import Event from "./events.js";
-import Job from "./jobs.js";
-import DonationCampaign from "./donations.js";
-import User from "./user.js";
-import College from "./college.js";
-import Post from "./post.js";
+import Event from './events.js';
+import Job from './jobs.js';
+import DonationCampaign from './donations.js';
+import User from './user.js';
+import College from './college.js';
+import Media from './media.js';
+import Transaction from './transactions.js';
+import Post from './post.js';
 
-export{
-    Event,
-    Job,
-    DonationCampaign,
-    User,
-    College,
-    Post
-}
+export { Event, Job, DonationCampaign, User, College, Media, Transaction , Post};
