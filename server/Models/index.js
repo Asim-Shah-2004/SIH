621a858ac8ced374f71b39131360325a71d3a1cc
asim <asimshah2111@gmail.com>
2024-12-09 20:40:14 +0530

    minor fix

---
+++ server/Models/index.js
@@ -0,0 +1,15 @@
+import Event from "./events.js";
+import Job from "./jobs.js";
+import DonationCampaign from "./donations.js";
+import User from "./user.js";
+import College from "./college.js";
+import Post from "./post.js";
+
+export{
+    Event,
+    Job,
+    DonationCampaign,
+    User,
+    College,
+    Post
+}
