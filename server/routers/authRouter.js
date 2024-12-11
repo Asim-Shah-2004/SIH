6e79dce3ce73aecf461da5494da66f5fcc92be89
asim <asimshah2111@gmail.com>
2024-12-11 22:35:03 +0530

    added college register

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { register, verify, login } from '../controllers/authController2.js';
+import {collegeRegister} from '../controllers/authController.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.get('/:email/verify', verify);
 authRouter.post('/login', login)
+authRouter.post('/collegeRegister', collegeRegister)
 
 export default authRouter;
