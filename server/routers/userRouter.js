99ad350b30897da2e2236c804bbc4c7a4b839c65
SayKnowToCode <ninadmaadhavi13@gmail.com>
2024-12-12 08:55:51 +0530

    College Login

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -15,15 +15,17 @@ import { authenticateToken } from '../middleware/authenticateToken.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/getConfig/:college_id',landingPageConfig);
+userRouter.get('/getConfig/:college_id', landingPageConfig);
 
-userRouter.get('/getAll',authenticateToken ,getAllUsers);
-userRouter.get('/getAllexCon', authenticateToken ,getAllUsers);
-userRouter.get('/donations',authenticateToken ,getDonations);
-userRouter.get('/getDepartments',authenticateToken ,getDepartments);
-userRouter.get('/fetch/:id',authenticateToken ,getUser);
-userRouter.post('/changePassword',authenticateToken ,changePassword)
-userRouter.post('/verifyPassword',authenticateToken ,verifyPassword)
+userRouter.get('/getAll', authenticateToken, getAllUsers);
+userRouter.get('/getAllexCon', authenticateToken, getAllUsers);
+userRouter.get('/donations', authenticateToken, getDonations);
+userRouter.get('/getDepartments', authenticateToken, getDepartments);
+userRouter.get('/fetch/:id', authenticateToken, getUser);
+userRouter.post('/changePassword', authenticateToken, changePassword)
+userRouter.post('/verifyPassword', authenticateToken, verifyPassword)
+userRouter.get('/fetchCollege/:id', authenticateToken, getCollege)
+
 
 
 export default userRouter;
